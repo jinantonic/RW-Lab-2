1b9c93d9176cf75da987085b79c539c3a1b8797d
Jina Park <jinapark119@gmail.com>
2022-10-24 02:50:04 +0100

    edit readme file

--- problem-1/phoneDirectory.js
+++ problem-1/phoneDirectory.js
@@ -1,51 +1,63 @@
-// Create an object Contacts
-// Define a function with 3 parameters and assign it to Contacts
-var Contact = function (contactName, mobileNumber, email) {
-    this.contactName = contactName;
-    this.mobileNumber = mobileNumber;
-    this.email = email;
-}
+document.addEventListener("DOMContentLoaded", function(event) {
+    var table = document.getElementById("contactsTable");
+    var rows = table.getElementsByTagName("tr");
+    var buttons = table.getElementsByTagName("button");
+    // var newbutton = document.getElementById("addButton");
+    var nameBox = document.getElementById("contactName");
+    var numberBox = document.getElementById("mobileNumber");
+    var emailBox = document.getElementById("email");
 
-var contactArr = []; // Declaring an array for the summary of the contacts
-contactArr.push(new Contact("Jina Park", "0838103838", "jinapark119@gmail.com"));
-contactArr.push(new Contact("Ovid Horan", "08234", "jinapark119@gl.com"));
-contactArr.push(new Contact("Skittles Park", "0234235344", "jinaasfad"));
+    function createIndex(){
+        for (var i = 0; i < rows.length; i++) {
+            rows[i].dataset.index = i;
+            if(i > 0){
+                rows[i].firstElementChild.innerHTML = i;
+            }
+        }
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].dataset.index = i+1;
+        }
+    }
 
-var contactList = function() {
-    var cont = document.getElementById("contactsTable");
-    
-    cont.innerHTML = '<tr><th>Name</th><th>Mobile</th><th>Email</th></tr>'; // Fixed header of the table
-    //cont.innerHTML = '<thead><tr><th>Name</th><th>Mobile</th><th>Email</th></tr></thead>'; // Fixed header of the table
+    function createCell(row, i, value) {
+        let cell = row.insertCell(i);
+        cell.innerHTML = value;
+    }
 
-    for (var i = 0; i < contactArr.length; i++) {
-        cont.innerHTML += '<tr><td id = "contactName' + i + '">' + contactArr[i].contactName + '</td>'
-                        + '<td id = "mobileNumber' + i + '">' + contactArr[i].mobileNumber + '</td>'
-                        + '<td id = "email' + i + '">' + contactArr[i].email + '</td></tr>';
-    } // end for 
+    function addRow() {
+        if(nameBox.value !== '' && numberBox.value !== '' && emailBox.value !== '') {
+            let row = table.insertRow(-1);
+            createCell(row,0,'');
+            createCell(row,1,nameBox.value);
+            createCell(row,2,numberBox.value);
+            // createCell(row,3,'<button class="delete">Delete</button>');
+            createCell(row,3,emailBox.value);
+            createIndex();
+            nameBox.value = '';
+            numberBox.value = '';
+            emailBox.value = '';
+        }
+        else{
+            alert('Please enter data!');
+        }
+    }
 
-    // var oddRow = document.querySelectorAll('tr:nth-child(odd)');
-    // for (let i = 0; i < oddRow.length; i++) {
-    //     oddRow[i].style.backgroundColor = 'red';
-    // } // end for
-} // end function
+    // function deleteRow(index){
+    //     let row = document.querySelector('[data-index="'+index+'"]');
+    //     row.parentNode.removeChild(row);s
+    //     createIndex();
+    // }
 
+    var clickEvent = document.addEventListener('click',function(event){
+        // if(event.target.classList.contains('delete')){
+        //     deleteRow(event.target.getAttribute('data-index'));
+        // }
+        if(event.target.id === 'addButton'){
+            addRow();
+        }
+        event.preventDefault();
+    });
 
-var addContact = function() {
-    var contactName = document.getElementById("contactName").value;
-    var mobileNumber = document.getElementById("mobileNumber").value;
-    var email = document.getElementById("email").value;
+    createIndex();
 
-    var addedContact = new Contact(contactName, mobileNumber, email)
-    contactArr.push(addedContact); // Add the new contact detail to the contactsSummary array
-    contactList(); // Call the function
-} // end function
-
-contactList();
-
-// function validateName(contactName) {
-//     var val = Boolean(contactName?.match(/^[A-Za-z\s]*$/));
-//     if(!val) {
-//         alert('eh');
-    
-//     }
-// }
+});
