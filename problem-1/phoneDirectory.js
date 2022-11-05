b0a54aeec959e2187e6cbbfd8171a6f422d5ccdf
Jina Park <jinapark119@gmail.com>
2022-11-05 00:09:58 +0000

    edit code

--- problem-1/phoneDirectory.js
+++ problem-1/phoneDirectory.js
@@ -65,25 +65,81 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 
 
+// // Name validation -> should contain only alphabets and space
+// function checkName(contactName) {
+//     var regexName = /^[a-zA-Z ]*$/;
 
-function checkName(contactName) {
-    var regexName = /^[a-z][a-z\s]*$/;
-    if(contactName.value.match(regexName)) {
-        return true;
-    }
-    else {
-        alert('Please enter a character or a space only for the contact name.');
-        return false;
-    }
-}
+//     if(contactName.value.match(regexName)) {
+//         return true;
+//     } // end if
 
-function checkNumber(contactNumber) {
-    var regexNumber = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
-    if(checkNumber.value.match(regexNumber)) {
-        return true;
-    }
-    else {
-        alert('Mobile number should contain only numbers, nothing else.');
+//     else {
+//         alert('.');
+//         return false;
+//     } // end else
+// } // end function checkName
+
+// // Mobile number validation -> should contain only numbers
+// function checkNumber(contactNumber) {
+//     var regexNumber = /^[0-9]*$/;
+
+//     if(contactNumber.value.match(regexNumber)) {
+//         return true;
+//     } // end if
+
+//     else {
+//         alert('Mobile number should contain only numbers, nothing else.');
+//         return false;
+//     } // end else
+// }
+
+// /* Email validation
+//    - Upper case and lower case letters (A-Z, a-z)
+//    - Numeric characters (0-9)
+//    - Special characters (- ! # $ % & ' * + - / = ? ^ _ ` { | } ~
+//    - Period, dot, or full stop (.) with the condition that it cannot be the first or last letter of the email and cannot repeat one after another
+// */
+// function checkEmail(email) {
+//     var regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+//     if(email.value.match(regexEmail)) {
+//         return true;
+//     } // end if
+
+//     else {
+//         alert('Invalid email address');
+//         return false;
+//     } // end else
+// } // end function checkEmail
+
+function validation() {
+    var contactName = document.forms["contact_input"]["contactName"].value;
+    var mobileNumber = document.forms["contact_input"]["mobileNumber"].value;
+    var email = document.forms["contact_input"]["email"].value;
+    var regexName = /^[a-zA-Z ]*$/;
+    var regexNumber = /^[0-9]*$/;
+    var regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if(contactName == "" || mobileNumber == "" || email == "") { // If all the fields are empty,
+        emptyError = "All fields must be entered."
+        document.getElementById("emptyError").innerHTML = emptyError; // display an error 
         return false;
-    }
-}
+    } // end if
+    
+    if (!contactName.match(regexName)) { 
+        nameError = "Please enter a character or a space only for the contact name."
+        document.getElementById("nameError").innerHTML = nameError; // display an error 
+        return false;
+    } // end if
+
+
+
+    return true;
+
+
+
+
+
+} // end function validation
+
+/* <button id = "addButton" onclick = "checkName(document.contact_input.contactName); checkNumber(document.contact_input.mobileNumber); checkEmail(document.contact_input.email);">Add Contact</button> */
