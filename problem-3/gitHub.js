daddbb7c4fab051a50997ce7e84faea69411ea11
Jina Park <jinapark119@gmail.com>
2022-11-06 20:56:41 +0000

    add code

--- problem-3/gitHub.js
+++ problem-3/gitHub.js
@@ -6,14 +6,12 @@ function getData() {
 	.then(data => {
 		console.log(data);
 		document.getElementById('profile').innerHTML = `<img src="${data.avatar_url}" />`
-		document.getElementById('name').innerHTML = "Name : " + data.name;
-		document.getElementById('login').innerHTML = "User Name : " + data.login;
-		document.getElementById('bio').innerHTML = "Bio : " + data.bio;
-        document.getElementById('email').innerHTML = "Email : " + data.email;
-		document.getElementById('location').innerHTML = "Location : " + data.location;
-		
-		document.getElementById('gists').innerHTML = "Number of Gists : " + data.public_gists;
-
+		document.getElementById('name').innerHTML = data.name;
+		document.getElementById('login').innerHTML = data.login;
+		document.getElementById('bio').innerHTML = data.bio;
+        document.getElementById('email').innerHTML = data.email + " email";
+		document.getElementById('location').innerHTML = data.location;
+		document.getElementById('gists').innerHTML = data.public_gists;
 	}))
 
 	function fetchRepos(username) {
@@ -40,8 +38,11 @@ function getData() {
 	})();
 	  
 	const addRows = (rows) => {
+		const tbody = document.querySelector('#repoTable tbody')
+		if(tbody.hasChildNodes()){
+			myNode.innerHTML = '';
+		}
 		rows.forEach(e => {
-			const tbody = document.querySelector('#repoTable tbody')
 			const tr = document.createElement('tr')
 			tr.innerHTML = rowHtml(e)
 			tbody.appendChild(tr)
@@ -55,11 +56,4 @@ function getData() {
 		return html
 	}
 
-	// function displayH() {
-	// 	var h2 = document.getElementsByTagName('h2');
-	// 	h2.style.display = 'inline';
-	// }
 }
-
-getData();
-
