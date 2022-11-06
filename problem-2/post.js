989380b179ab0714084cbb74d3d318dde45eb7a9
Jina Park <jinapark119@gmail.com>
2022-11-06 20:12:52 +0000

    add code

--- problem-2/post.js
+++ problem-2/post.js
@@ -1,7 +1,7 @@
 const fetchPosts = async() => {
     return fetch("https://jsonplaceholder.typicode.com/posts") // Call the fetch function
     .then(response => response.json()) // .json() method parses the JSON response into a JS object literal, response type
-    //.then(data => console.log(data)); //log the data;
+    //.then(data => console.log(data)); // Log the data;
 } 
 
 const queries = async() => {
@@ -10,13 +10,16 @@ const queries = async() => {
     // ans = Array.isArray(posts); // Check is posts is an array or not
     // console.log(ans); // Returns true
 
-    // function countTitle(value) {
+    /* 1st way to display the contents including userId, id, title and body */
+    // function countTitle(value) { // Function to count the number of words in the title
     //     return value.title.split(' ').length > 6
-    // }
+    // } // end function countTitle
 
+    
     // var filterTitle = posts.filter(countTitle)
     // console.log(filterTitle); 
 
+    /* 2nd way to display the contents including only titles in an organised form */
     // posts.forEach((posts) => {
     //     var numWords = posts.title.split(' ').length;
     //     if (numWords > 6) {
@@ -24,23 +27,24 @@ const queries = async() => {
     //     }
     // });
 
+    /* 3rd way to display the contents including only titles in a plain text */
     // posts.forEach(function(posts) {
     //     var numWords = posts.title.split(' ').length;
     //     // console.log(numWords); // Display the number of words in each title
-    //     // console.log(posts.title);
+    //     // console.log(posts.title); // Display all of the titiles
     //     if (numWords > 6) { // List all of the post titles having more than 6 words
     //         console.log(posts.title);
     //     }
-    // });
-
+    // }); 
+    
     const wordFreq = function (wordBody, word) {
-        if (wordBody.has(word)) {
+        if (wordBody.has(word)) { // If the word exists in the title
             wordBody.set(word, wordBody.get(word) + 1)
         } else {
             wordBody.set(word, 1)
         } // end if else
         return wordBody
-    }
+    } 
       
     const wordCount = posts => [...posts
         .flatMap(item => item.body.split(/\s+/))
@@ -49,7 +53,6 @@ const queries = async() => {
         .sort((a, b) => b[1] - a[1])
         // .slice(0, 5)
         .map(([word, count]) => `${word} [${count}]`)
-        // .join(', ')
     console.log(wordCount(posts))
 }
 
